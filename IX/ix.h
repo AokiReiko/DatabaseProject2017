3ca4c1a73285b98f2584e4c30c4512bf01a35f55
zfw <zfw15@mails.tsinghua.edu.cn>
2017-11-05 06:42:22 -0800

    builded the bone of indexhandle

--- IX/ix.h
+++ IX/ix.h
@@ -8,13 +8,16 @@
 #define IX_H
 
 #include "../database.h"
-#include "RM/rm.h"
+#include "../RM/rm.h"
 #include "../filesystem/bufmanager/BufPageManager.h"
 #include "../filesystem/fileio/FileManager.h"
 #include "../filesystem/utils/pagedef.h"
 #include <string>
 #include <stdlib.h>
 #include <cstring>
+
+//entries are stored like this:
+//(RID id, char status, AttrType attr)
 
 struct IX_IndexHeader{
     AttrType attr_type; // attribute type and length
@@ -29,6 +32,87 @@ struct IX_IndexHeader{
     int maxKeys_B;
 
     PageNum rootPage;   // Page number associated with the root page
+};
+
+struct IX_NodeHeader{   // general header for node pages
+    bool isLeafNode;  // indicator for whether it is a leaf node
+    bool isEmpty;     // Whether the node contains pointers or not
+    int num_keys;     // number of valid keys the node holds
+  
+    int firstSlotIndex; // the pointer to the beginning of the linked list of
+                        // valid key/pointer slots
+    int freeSlotIndex;  // the pointer to the beginning of free slots
+    PageNum invalid1;
+    PageNum invalid2;
+};
+  
+struct IX_NodeHeader_I{   // header for internal pages
+    bool isLeafNode;
+    bool isEmpty;     // whether the node has its first page pointer
+                      // or not
+    int num_keys;
+  
+    int firstSlotIndex; 
+    int freeSlotIndex;
+    PageNum firstPage; // first leaf page under this internal node
+    PageNum invalid2;
+};
+  
+struct IX_NodeHeader_L{   //header for leaf pages
+    bool isLeafNode;
+    bool isEmpty; 
+    int num_keys;
+  
+    int firstSlotIndex;
+    int freeSlotIndex;
+    PageNum nextPage; // next leaf page
+    PageNum prevPage; // previous leaf page
+};
+  
+struct IX_BucketHeader{     // header for the bucket pages
+    int num_keys;         // number of entries in the bucket
+    int firstSlotIndex;   // pointer to the first valid slot in the bucket
+    int freeSlotIndex;    // pointer to the first free slot in bucket
+    PageNum nextBucket;   // pointer to the next bucket page (in case of 
+                          // overflow, i.e. lots of duplicates)
+};
+
+class IX_IndexHandle {
+    friend class IX_Manager;
+public:
+     IX_IndexHandle  ();                             // Constructor
+     ~IX_IndexHandle ();                             // Destructor
+    RC InsertEntry     (void *pData, const RID &rid);  // Insert new index entry
+    RC DeleteEntry     (void *pData, const RID &rid);  // Delete index entry
+    RC ForcePages      ();                             // Copy index to disk
+
+private:
+    BufPageManager* BPM;
+    BufType rootPage;
+    int rootIndex;
+    struct IX_IndexHeader header;
+    int insertEntryIntoPage(void *pData, const RID &rid, BufType curPage, int pageIndex);
+    int deleteEntryFromPage(void *pData, const RID &rid, BufType curPage, int pageIndex);
+    RC getDataAt(BufType buf, int index, char* &p);
+    RC getPointerAt(BufType buf, int index, char* &p);
+    RC getStatusAt(BufType buf, int index, char* &p);
+    BufType getPageByIndex(int place, BufType curPage, int &childPageIndex);
+    int getPlace(void *pData, BufType curPage);
+    int getExactPlace(void *pData, BufType curPage);
+    int calcSize(BufType curPage,int place);
+    bool isDataEqual(void *pData, void* comp);
+};
+
+class IX_IndexScan {
+public:
+     IX_IndexScan  ();                                 // Constructor
+     ~IX_IndexScan ();                                 // Destructor
+  RC OpenScan      (const IX_IndexHandle &indexHandle, // Initialize index scan
+                    CompOp      compOp,
+                    void        *value,
+                    ClientHint  pinHint = NO_HINT);           
+  RC GetNextEntry  (RID &rid);                         // Get next matching entry
+  RC CloseScan     ();                                 // Terminate index scan
 };
 
 class IX_Manager {
@@ -47,23 +131,4 @@ public:
   RC CloseIndex   (IX_IndexHandle &indexHandle);  // Close index
 };
 
-class IX_IndexHandle {
-public:
-     IX_IndexHandle  ();                             // Constructor
-     ~IX_IndexHandle ();                             // Destructor
-  RC InsertEntry     (void *pData, const RID &rid);  // Insert new index entry
-  RC DeleteEntry     (void *pData, const RID &rid);  // Delete index entry
-  RC ForcePages      ();                             // Copy index to disk
-};
-
-class IX_IndexScan {
-public:
-     IX_IndexScan  ();                                 // Constructor
-     ~IX_IndexScan ();                                 // Destructor
-  RC OpenScan      (const IX_IndexHandle &indexHandle, // Initialize index scan
-                    CompOp      compOp,
-                    void        *value,
-                    ClientHint  pinHint = NO_HINT);           
-  RC GetNextEntry  (RID &rid);                         // Get next matching entry
-  RC CloseScan     ();                                 // Terminate index scan
-};
+#endif
--- IX/ix_indexhandle.cpp
+++ IX/ix_indexhandle.cpp
@@ -1,2 +1,167 @@
 #include "ix.h"
 #include <memory.h>
+
+IX_IndexHandle::IX_IndexHandle(){
+
+}
+
+IX_IndexHandle::~IX_IndexHandle(){
+    
+}
+
+RC IX_IndexHandle::InsertEntry(void *pData, const RID &rid){
+    return insertEntryIntoPage(pData, rid, rootPage, rootIndex);
+}
+
+RC IX_IndexHandle::DeleteEntry(void *pData, const RID &rid){
+    return deleteEntryFromPage(pData, rid, rootPage, rootIndex);
+}
+
+RC IX_IndexHandle::ForcePages(){
+
+}
+
+RC IX_IndexHandle::getDataAt(BufType buf, int index, char* &p){
+    if (index >= header.maxKeys_N) {
+        return -1;
+    }
+    p = (char*)buf + header.entryOffset_N + index*(header.attr_length+1+sizeof(RID))+sizeof(RID)+1;
+    return 0;
+}
+
+RC IX_IndexHandle::getPointerAt(BufType buf, int index, char* &p){
+    if (index >= header.maxKeys_N) {
+        return -1;
+    }
+    p = (char*)buf + header.entryOffset_N + index*(header.attr_length+1+sizeof(RID));
+    return 0;
+}
+
+RC IX_IndexHandle::getStatusAt(BufType buf, int index, char* &p){
+    if (index >= header.maxKeys_N) {
+        return -1;
+    }
+    p = (char*)buf + header.entryOffset_N + index*(header.attr_length+1+sizeof(RID))+sizeof(RID);
+    return 0;
+}
+
+int IX_IndexHandle::insertEntryIntoPage(void *pData, const RID &rid, BufType curPage, int pageIndex){
+    IX_NodeHeader* pheader = (IX_NodeHeader*) curPage;
+    if (pheader->isLeafNode){
+        int place = getPlace(pData, curPage);
+        int sizeToMove = calcSize(curPage,place);
+        char* placeholder;
+        char* dataStored;
+        char* statusStored;
+        getPointerAt(curPage,place,placeholder);
+        getDataAt(curPage,place,dataStored);
+        getStatusAt(curPage,place,statusStored);
+        if (!(isDataEqual(pData,dataStored))){
+            char* tmp = (char*)malloc(sizeToMove);
+            memcpy(tmp, placeholder, sizeToMove);
+            memcpy(placeholder+header.attr_length+1+sizeof(RID), tmp, sizeToMove);
+            memcpy(placeholder,&rid,sizeof(RID));
+            char c = 'd';
+            memcpy(statusStored,&c,1);
+            memcpy(dataStored,pData,header.attr_length);
+            pheader->num_keys++;
+            BPM->markDirty(pageIndex);
+
+            if (pheader->num_keys > header.maxKeys_N){
+                return 1;   //notify parent about overflow
+            } else {
+                return 0;
+            }
+        } else if (*(statusStored) == 'd'){
+            // add bucket page
+        } else if (*(statusStored) == 'b'){
+            // add entry into bucket page
+        } else {
+            return -1;
+        }
+    } else {
+        int place = getPlace(pData, curPage);
+        int childPageIndex;
+        BufType childPage = getPageByIndex(place, curPage, childPageIndex);
+        int result = insertEntryIntoPage(pData, rid, childPage, childPageIndex);
+        if (result == 0){
+            return 0;
+        } else if (result == 1){
+            BPM->markDirty(pageIndex);
+            //deal with overflow
+            if (pageIndex == rootIndex){
+                //deal with changed root
+            }
+        }
+    }
+
+    return -1;
+}
+
+int IX_IndexHandle::deleteEntryFromPage(void *pData, const RID &rid, BufType curPage, int pageIndex){
+    IX_NodeHeader* pheader = (IX_NodeHeader*) curPage;
+    if (pheader->isLeafNode){
+        int place = getExactPlace(pData, curPage);
+        if (place == -1) {
+            return -1;
+        }
+        int sizeToMove = calcSize(curPage,place+1);
+        char* placeholder;
+        char* statusStored;
+        getPointerAt(curPage,place,placeholder);
+        getStatusAt(curPage,place,statusStored);
+        if (*(statusStored) == 'd'){
+            char* tmp = (char*)malloc(sizeToMove);
+            memcpy(tmp, placeholder+header.attr_length+1+sizeof(RID), sizeToMove);
+            memcpy(placeholder, tmp, sizeToMove);
+            BPM->markDirty(pageIndex);
+            pheader->num_keys--;
+            if (pheader->num_keys < header.maxKeys_N/2){
+                return 1;   //notify parent about underflow
+            }
+            else {
+                return 0;
+            }
+        } else if (*(statusStored) == 'b'){
+            // delete entry from bucket page
+        } else {
+            return -1;
+        }
+    } else {
+        int place = getPlace(pData, curPage);
+        int childPageIndex;
+        BufType childPage = getPageByIndex(place, curPage, childPageIndex);
+        int result = insertEntryIntoPage(pData, rid, childPage, childPageIndex);
+        if (result == 0){
+            return 0;
+        } else if (result == 1){
+            BPM->markDirty(pageIndex);
+            //deal with underflow
+            if (pageIndex == rootIndex){
+                //deal with changed root
+            }
+        }
+    }
+
+    return -1;
+}
+
+BufType IX_IndexHandle::getPageByIndex(int place, BufType curPage, int &childPageIndex){
+
+}
+
+int IX_IndexHandle::getPlace(void *pData, BufType curPage){
+    
+}
+
+int IX_IndexHandle::getExactPlace(void *pData, BufType curPage){
+
+}
+
+int IX_IndexHandle::calcSize(BufType curPage,int place){
+
+}
+
+bool IX_IndexHandle::isDataEqual(void *pData, void* comp){
+    
+}
